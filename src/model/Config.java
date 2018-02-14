af2d8ad024699ff830653d9db8fb2596defd2ad6
mrityunjayfaujdar <mrityunjayfaujdar@gmail.com>
2018-02-14 15:16:11 +0530

    Added configuration files xls, config.java

---
+++ src/model/Config.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Config {
+	
+	String browserName ; 
+	String baseURL ;
+	
+	
+	public String getBrowserName() {
+		return browserName;
+	}
+	public void setBrowserName(String browserName) {
+		this.browserName = browserName;
+	}
+	public String getBaseURL() {
+		return baseURL;
+	}
+	public void setBaseURL(String baseURL) {
+		this.baseURL = baseURL;
+	} 
+
+}
