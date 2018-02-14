6776d0868a1a546e8b8b6c9b02002a9fcc1f3c8c
mrityunjayfaujdar <mrityunjayfaujdar@gmail.com>
2018-02-14 15:53:10 +0530

    Home: click sports, click movies

--- src/pages/Home.java
+++ src/pages/Home.java
@@ -1,5 +1,20 @@
 package pages;
 
-public class Home {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class Home {
+	
+	WebDriver driver ; 	
+	public final WebElement sportsLink = driver.findElement(By.xpath("//a[contains(text(),'SPORTS')]"));
+	public final WebElement moviesLink = driver.findElement(By.xpath("//a[contains(text(),'MOVIES')]"));
+	
+	public void clickSports() {
+			sportsLink.click();
+	}
+	
+	public void clickMovies() {
+		moviesLink.click();
+}
 }
